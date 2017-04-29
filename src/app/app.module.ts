6aca824d9e6b3a7ee1395eac5ee381e241065d43
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-29 18:21:26 +0200

    Add camera dep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { File } from '@ionic-native/file';
+import { Camera } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -35,6 +37,8 @@ import { firebaseConfig } from './app.firebase.conf.ts'
   providers: [
     StatusBar,
     SplashScreen,
+    File,
+    Camera,
     { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
